25f7008382c07d1d28a5e0c288689f5b5268731c
Kyle Morton <mkmorton91@gmail.com>
2025-08-04 17:54:40 -0500

    Few missed things on starting up the capture

--- Barcode-Scanner/ScannerViewController.swift
+++ Barcode-Scanner/ScannerViewController.swift
@@ -34,6 +34,22 @@ final class ScannerVC: UIViewController {
         fatalError("init(coder:) has not been implemented") // only required for storyboard (not using it)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCaptureSession()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        guard let previewLayer = previewLayer else {
+            scannerDelete?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        previewLayer.frame = view.layer.bounds
+    }
+    
     private func setupCaptureSession() {
         
         guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
